a59ecef7aa79fafecaa5c00f4b931e93dbc4fdd0
Jack-Tillman <johntillman345@gmail.com>
2023-09-21 12:16:01 -0400

    Created Home component that is functional, removed unnecessary parts of starter App.jsx, imported Home into App and placed in render

---
+++ StrangersThings/src/components/Home.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { fetchAllPosts } from "../API";
+
+export const Home = () => {
+    // state to keep all posts, useful to render all posts 
+    const [posts, setPosts] = useState([]);
+    // state to track any errors, useful for conditional rendering
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function getAllPosts() {
+          const APIReesponse = await fetchAllPosts();
+          console.log(APIReesponse);
+          if (APIReesponse.success) {
+            setPosts(APIReesponse.data.posts);
+          } else {
+            setError(APIReesponse.error.message);
+          }
+        }
+        getAllPosts();
+      }, []);
+
+    return (
+       <> 
+       {/*conditionally render the errors only if there are errors in state*/}
+       {error && <p className="error-notification">{error.message}</p>}
+       {/*conditionally render the posts only if there are posts in state*/}
+          {posts && 
+            <div id="all-post-container">
+                {posts.map((post) => {
+                    return (
+                        <div id="post-container" key={post._id}>
+                          <h2>{post.title}</h2>
+                          <h3>{post.author.username}</h3>
+                          <p>{post.description}</p>
+                          <p>{post.price}</p>
+                          <p>{post.createdAt}</p>
+                          <p>{post.updatedAt}</p>
+                          <p>{post.location}</p>
+                          <p>{post.willDeliver}</p>
+                        </div>
+                    )
+                })}
+            </div>}
+       </>
+    )
+}
