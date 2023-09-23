b9b4acb509370987eeaf2f66842c76bb214e6568
adrienne365 <adrienne365@gmail.com>
2023-09-23 15:50:37 -0400

    updated Register to match main

--- StrangersThings/src/components/Register.jsx
+++ StrangersThings/src/components/Register.jsx
@@ -1,27 +1,119 @@
-import { useState } from 'react'
-import { registerUser } from '../API/index'
+import { useState } from "react";
+import { registerUser } from "../API/index";
 
+const Register = ({ token, setToken }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-const Register = () => {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [token, setToken] = useState("")
-    const [authenticated, setAuthenticated] = useState(null)
+  //   const [token, setToken] = useState("");
 
-    async function handleUserRegistration(e) {
-        e.preventDefault();
-        const newUser = await registerUser;
-        if (e.target.password === e.target.confirmPassword) 
-        authenticated;
-    return (
-        <form onSubmit={handleUserRegistration}>
-            <label>
-                <input type="text" />
-            </label>
-                <input type="submit" value="Submit" />
-        </form>
-        
-    )}
-}
+  function handleUserRegistration(e) {
+    e.preventDefault();
+
+    const fetchToken = async (username, password) => {
+      try {
+        const newUser = await registerUser(username, password);
+        console.log(newUser);
+        if (newUser.success) {
+          setUsername("");
+          setPassword("");
+          setConfirmPassword("");
+          return newUser;
+        } else {
+          console.log(newUser);
+          return newUser;
+        }
+      } catch (error) {
+        console.error(error, error.message);
+      }
+    };
+    fetchToken(username, password);
+  }
+
+  return (
+    <>
+      <form>
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={username}
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            minLength="5"
+            maxLength="20"
+            value={password}
+            required
+            autoComplete="off"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label htmlFor="confirmPassword">
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Confirm Password"
+            minLength="5"
+            maxLength="20"
+            value={confirmPassword}
+            required
+            autoComplete="off"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button onClick={handleUserRegistration}>Register Me</button>
+        </label>
+      </form>
+    </>
+  );
+};
 
 export default Register;
+
+/* 
+
+
+
+
+  <form
+          onSubmit={handleUserRegistration}
+          style={{ width: "1200px", height: "1200px" }}
+        >
+          <label htmlFor="username">
+            Username:
+            <input
+              type="text"
+              name="username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </label>
+          <button className="register-btn" type="submit">
+            Register
+          </button>
+        </form>
+
+
+
+*/
