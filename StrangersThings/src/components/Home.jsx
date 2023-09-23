c01e84fa8052ba3a5962d5d67e09012d1417e76a
Jack-Tillman <johntillman345@gmail.com>
2023-09-23 14:23:13 -0400

    added classes to home elements

--- StrangersThings/src/components/Home.jsx
+++ StrangersThings/src/components/Home.jsx
@@ -33,25 +33,25 @@ const Home = ({ token }) => {
           {posts.map((post) => {
             return (
               <div id="post-container" key={post._id}>
-                {/*I gave each element within post-container class of post as well as specific classname so that */}
-                <h2 className="post-h2">{post.title}</h2>
-                <h3 className="post-h3">{post.author.username}</h3>
-                <p className="post-description">{post.description}</p>
-                <p className="post-price">{post.price}</p>
-                <p className="post-createdAt">{post.createdAt}</p>
-                <p className="post-updatedAt">{post.updatedAt}</p>
-                <p className="post-location">{post.location}</p>
-                <p className="post-willDeliver">{post.willDeliver}</p>
-                <button
-                className="details-button"
-                id="details-button"
-                  onClick={() => {
-                    //when clicked, take user to detailed post view which will need to use useParams to render the post
-                    navigate(`/${post._id}`);
-                  }}
-                >
-                  See Details
-                </button>
+              {/*I gave each element within post-container class of post as well as specific classname so that */}
+              <h2 className="post-h2">{post.title}</h2>
+              <h3 className="post-h3">{post.author.username}</h3>
+              <p className="post-description">{post.description}</p>
+              <p className="post-price">{post.price}</p>
+              <p className="post-createdAt">{post.createdAt}</p>
+              <p className="post-updatedAt">{post.updatedAt}</p>
+              <p className="post-location">{post.location}</p>
+              <p className="post-willDeliver">{post.willDeliver}</p>
+              <button
+              className="details-button"
+              id="details-button"
+                onClick={() => {
+                  //when clicked, take user to detailed post view which will need to use useParams to render the post
+                  navigate(`/${post._id}`);
+                }}
+              >
+                See Details
+              </button>
               </div>
             );
           })}
