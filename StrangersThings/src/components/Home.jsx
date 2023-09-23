fab80e5c47c7b98b5125cd874af409134718f9e5
Jack-Tillman <johntillman345@gmail.com>
2023-09-23 14:18:11 -0400

    added classes to post elements within Home

--- StrangersThings/src/components/Home.jsx
+++ StrangersThings/src/components/Home.jsx
@@ -33,21 +33,24 @@ const Home = ({ token }) => {
           {posts.map((post) => {
             return (
               <div id="post-container" key={post._id}>
-                <h2>{post.title}</h2>
-                <h3>{post.author.username}</h3>
-                <p>{post.description}</p>
-                <p>{post.price}</p>
-                <p>{post.createdAt}</p>
-                <p>{post.updatedAt}</p>
-                <p>{post.location}</p>
-                <p>{post.willDeliver}</p>
+                {/*I gave each element within post-container class of post as well as specific classname so that */}
+                <h2 className="post-h2">{post.title}</h2>
+                <h3 className="post-h3">{post.author.username}</h3>
+                <p className="post-description">{post.description}</p>
+                <p className="post-price">{post.price}</p>
+                <p className="post-createdAt">{post.createdAt}</p>
+                <p className="post-updatedAt">{post.updatedAt}</p>
+                <p className="post-location">{post.location}</p>
+                <p className="post-willDeliver">{post.willDeliver}</p>
                 <button
+                className="details-button"
+                id="details-button"
                   onClick={() => {
                     //when clicked, take user to detailed post view which will need to use useParams to render the post
                     navigate(`/${post._id}`);
                   }}
                 >
-                  Edit post
+                  See Details
                 </button>
               </div>
             );
