6f82e7f5a4de0e80da976ad52f393f992dfde9df
Sam Burnett <maisterburns@gmail.com>
2023-09-26 10:57:10 -0500

    ...

--- StrangersThings/src/components/FetchMeData.jsx
+++ StrangersThings/src/components/FetchMeData.jsx
@@ -1,26 +1,22 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-
-export default async function FetchMeData(){
-    const token = sessionStorage.getItem(token)
-    
-  
-        try {
-            console.log(token)
-            const response = await fetch(`https://strangers-things.herokuapp.com/api/2306-ftb-et-web-am/users/me`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-              });
-                const result = await response.json();
-                console.log(result);
-                return result
-        }
-             catch (err) {
-                console.error(err)
-            }
-        }
-
-    
+export default async function FetchMeData() {
+  const token = sessionStorage.getItem;
+  try {
+    const response = await fetch(
+      `https://strangers-things.herokuapp.com/api/2306-ftb-et-web-am/users/me`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
