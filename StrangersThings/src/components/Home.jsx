d088d36b32c38cf721ce9a3b82ecd22748795f2e
Jack-Tillman <johntillman345@gmail.com>
2023-09-26 13:04:17 -0400

    Added PostListName component, made filter search bar functional

--- StrangersThings/src/components/Home.jsx
+++ StrangersThings/src/components/Home.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import { fetchAllPosts } from "../API";
 import { useNavigate } from "react-router-dom";
 import Searchbar from "./Searchbar";
+import PostListName from "./PostListName";
 
 const Home = ({ token }) => {
   // state to keep all posts, useful to render all posts
   const [posts, setPosts] = useState([]);
   // state to track any errors, useful for conditional rendering
   const [error, setError] = useState(null);
+  // state to track user input for search bar
+  const [searchParam, setSearchParam] = useState("");
 
   const navigate = useNavigate();
 
@@ -24,37 +27,30 @@ const Home = ({ token }) => {
     getAllPosts();
   }, []);
 
+  const postsToDisplay = searchParam
+    ? posts.filter((post) => post.title.toLowerCase().includes(searchParam))
+    : posts;
+
   return (
     <>
       {/*conditionally render the errors only if there are errors in state*/}
       {error && <p className="error-notification">{error.message}</p>}
       {/*conditionally render the posts only if there are posts in state*/}
-      {posts && (
+
+      <div id="searchbar-container">
+        <label htmlFor="searchbar">
+          Search Posts:{" "}
+          <input
+            type="text"
+            placeholder="search"
+            onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
+          />
+        </label>
+      </div>
+      {postsToDisplay && (
         <div id="all-post-container">
-          {posts.map((post) => {
-            return (
-              <div id="post-container" key={post._id}>
-                {/*I gave each element within post-container class of post as well as specific classname so that */}
-                <h2 className="post-h2">{post.title}</h2>
-                <h3 className="post-h3">{post.author.username}</h3>
-                <p className="post-description">{post.description}</p>
-                <p className="post-price">{post.price}</p>
-                <p className="post-createdAt">{post.createdAt}</p>
-                <p className="post-updatedAt">{post.updatedAt}</p>
-                <p className="post-location">{post.location}</p>
-                <p className="post-willDeliver">{post.willDeliver}</p>
-                <button
-                  className="details-button"
-                  id="details-button"
-                  onClick={() => {
-                    //when clicked, take user to detailed post view which will need to use useParams to render the post
-                    navigate(`/${post._id}`);
-                  }}
-                >
-                  See Details
-                </button>
-              </div>
-            );
+          {postsToDisplay.map((post) => {
+            return <PostListName key={post.id} post={post} />;
           })}
         </div>
       )}
---
+++ StrangersThings/src/components/PostListName.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
+
+const PostListName = ({post}) => {
+    const navigate = useNavigate();
+  return (
+    <div id="post-container" >
+      {/*I gave each element within post-container class of post as well as specific classname so that */}
+      <h2 className="post-h2">{post.title}</h2>
+      <h3 className="post-h3">{post.author.username}</h3>
+      <p className="post-description">{post.description}</p>
+      <p className="post-price">{post.price}</p>
+      <p className="post-createdAt">{post.createdAt}</p>
+      <p className="post-updatedAt">{post.updatedAt}</p>
+      <p className="post-location">{post.location}</p>
+      <p className="post-willDeliver">{post.willDeliver}</p>
+      <button
+        className="details-button"
+        id="details-button"
+        onClick={() => {
+          //when clicked, take user to detailed post view which will need to use useParams to render the post
+          navigate(`/${post._id}`);
+        }}
+      >
+        See Details
+      </button>
+    </div>
+  );
+};
+
+export default PostListName;
