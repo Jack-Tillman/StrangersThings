8842c4a005ae790d7967fdc5897b339410fd08ab
Mai Leyda <mai.toyonishi@yahoo.com>
2023-09-26 10:37:02 -0500

    Updated DetailedPosts and DeletePosts

--- StrangersThings/src/components/DetailedPosts.jsx
+++ StrangersThings/src/components/DetailedPosts.jsx
@@ -1,1 +1,31 @@
-/* will also feature edit and remove posts, and also sending messages */
+/* will also feature details and remove posts */
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Post Details
+export default function DetailedPosts({ post }) {
+    const navigate = useNavigate();
+    return (
+        <div>
+          <h3>{post.name}</h3>
+          <button
+            onClick={() => {
+              navigate(`/${post.id}`);
+            }}
+          >
+            See Details
+          </button>
+        </div>
+      );
+}
+
+// Delete post
+async function handleDelete() {
+    try {
+        const result = await deletePost(post.id);
+        console.log(result);
+        navigate("/");
+    } catch (error) {
+        console.error(error);
+    }
+};
